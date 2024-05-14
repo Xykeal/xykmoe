b22a4d930abd0190cb71c7ccaa9e6883e197aaab
Xykeal <xykealmc@gmail.com>
2024-05-15 00:39:55 +1000

    moved spire dir

--- src/pages/Spire.jsx
+++ src/pages/Spire.jsx
@@ -2,7 +2,7 @@ import { Box, Button, TextField, Typography } from '@mui/material';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const home = 'src/assets/Spire/Home.html';
+const home = '/Spire/Home.html';
 
 const Spire = () => {
   document.title = 'Xykeal - Stuff'
